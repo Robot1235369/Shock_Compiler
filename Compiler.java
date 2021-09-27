5e434b8d51066d01b33c1b6053370bd80aaa83c6
Isaac <you@example.com>
2021-09-26 20:28:27 -0500

    working on try-catch statement

--- Compiler.java
+++ Compiler.java
@@ -1,20 +1,23 @@
 class Compiler {
     public static void main(String args[]) {
         try {
+            throw new AssertionError();
             double version = 1.0;
             if (args[0].equals("--help")) {
-                System.out.println("java Compiler.java [Squirrel_File_Name] : runs Squirrel files");
+                System.out.println("java Compiler.java [Shock_File_Name] : runs Shock files");
                 System.out.println("java Compiler.java --help : list of commands");
-                System.out.println("java Compiler.java --version : find current Squirrel version");
+                System.out.println("java Compiler.java --version : find current Shock version");
                 System.exit(0);
             } else if (args[0].equals("--version")) {
-                System.out.print("Squirrel ");
-                System.out.println(version);
-                System.exit(0);
+                System.out.print("Shock ");
+                System.out.print(version);
+                System.out.println(" Beta")
+                System.exit();
+            } else {
+                String file = args[0];
             }
-            String file = args[0];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //System.out.println(e);
+        } catch (Error e) {
+            System.out.println(getClass().getSimpleName(e));
         }
     }
 }
