ab941b9ab66d02967e4f6ea3c50c6fe1ad8d6868
Robot1235369 <IsaacCA2018@gmail.com>
2021-10-05 13:19:07 +0000

    working on variables

--- Compiler.java
+++ Compiler.java
@@ -36,50 +36,30 @@ class Compiler {
             int line = 0;
             String data = null;
 
-            // read first line of code, excluding the empty ones
             do {
-                line++;
-                data = read(data, line, Reader);
-            } while (data.equals(""));
 
-            // initializing array to hold a split version of the line currently being analized
-            String[] data_list = data.split(" ");
-
-            // code for if the first line of code is an import statement
-            if (data_list[0] == "import") {
-
-            // code for if the first line of code is the initialization of a variable or a function
-            } else if (data_list[0] == "int" || data_list[0] == "float" || data_list[0] == "String" || data_list[0] == "Boolean") {
-                // check if it is a function
-                if (data_list.asList().contains("(")) {
-                    String[] data1 = data_list[1];
-                    // the code will save the name of the function and everything inside it to a 2d array called functions
-                    String[] data2 = data_list[1].split("(");
-                    functions[x][y] = data2[0];
-                    y++;
-                    data = "{";
-                    while (data != "}") {
-                        if (data != "{") {
-                            functions[x][y] = data;
-                            y++;
-                        }
-                        data = read(data, line, Reader);
-                        line++;
-                    }
-                // if it is a variable
-                } else {
-
+                if (Reader.hasNextLine == false) {
+                    break;
                 }
-            // check if it is a function that starts with "void"
-            } else if (data_list[0] == "void") {
-                // check if it is the main function
-                if (data1[0] == "m" && data1[1] == "a" && data1[2] == "i" && data1[3] == "n") {
 
-                } else {
-                    String[] data1 = data_list[1];
-                    if (data1[0] == "m" && data1[1] == "a" && data1[2] == "i" && data1[3] == "n") {
+                // read first line of code, excluding the empty ones
+                do {
+                    line++;
+                    data = read(data, line, Reader);
+                } while (data.equals(""));
 
-                    } else {
+                // initializing array to hold a split version of the line currently being analized
+                String[] data_list = data.split(" ");
+
+                // code for if the first line of code is an import statement
+                if (data_list[0] == "import") {
+
+                // code for if the first line of code is the initialization of a variable or a function
+                } else if (data_list[0] == "int" || data_list[0] == "float" || data_list[0] == "String" || data_list[0] == "Boolean") {
+                    // check if it is a function
+                    if (data_list.asList().contains("(")) {
+                        String[] data1 = data_list[1];
+                        // the code will save the name of the function and everything inside it to a 2d array called functions
                         String[] data2 = data_list[1].split("(");
                         functions[x][y] = data2[0];
                         y++;
@@ -92,11 +72,40 @@ class Compiler {
                             data = read(data, line, Reader);
                             line++;
                         }
+                    // if it is a variable
+                    } else {
+
                     }
+                // check if it is a function that starts with "void"
+                } else if (data_list[0] == "void") {
+                    // check if it is the main function
+                    if (data1[0] == "m" && data1[1] == "a" && data1[2] == "i" && data1[3] == "n") {
+
+                    } else {
+                        String[] data1 = data_list[1];
+                        if (data1[0] == "m" && data1[1] == "a" && data1[2] == "i" && data1[3] == "n") {
+
+                        } else {
+                            String[] data2 = data_list[1].split("(");
+                            functions[x][y] = data2[0];
+                            y++;
+                            data = "{";
+                            while (data != "}") {
+                                if (data != "{") {
+                                    functions[x][y] = data;
+                                    y++;
+                                }
+                                data = read(data, line, Reader);
+                                line++;
+                            }
+                        }
+                    }
+                } else {
+                    error(line, data);
                 }
-            } else {
-                error(line, data);
-            }
+            
+            } while(Reader.hasNextLine());
+
             Reader.close();
         } catch (Error e) {
             //System.out.println(e);
@@ -120,6 +129,22 @@ class Compiler {
     }
 }
 
-puublclass variables {
+public class integer {
+    String name;
+    int value;
+}
 
+public class string {
+    String name;
+    String value;
+}
+
+public class Float {
+    String name;
+    float value;
+}
+
+public class integer {
+    String name;
+    int value;
 }
